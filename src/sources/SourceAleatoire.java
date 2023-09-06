913dd8350ec8220756a76f97da32fd6a36dddcd1
Tchadel Icard <tchadel.icard@imt-atlantique.net>
2023-09-06 21:22:28 +0200

    add javadoc to the classes we've implemented today

--- src/sources/SourceAleatoire.java
+++ src/sources/SourceAleatoire.java
@@ -3,8 +3,20 @@ package sources;
 import information.Information;
 import java.util.Random;
 
+/**
+ * Classe permettant de générer de l'information de type Boolean
+ * de manière aléatoire
+ *
+ * @see Source
+ */
 public class SourceAleatoire extends Source<Boolean> {
 
+    /**
+     * un constructeur permettant de générer une information binaire
+     * aléatoire, d'une certaine longueur de bits
+     *
+     * @param nbBitsMess le nombre de bits du message généré
+     */
     public SourceAleatoire(int nbBitsMess) {
         super();
         this.informationGeneree = new Information<Boolean>();
@@ -15,6 +27,13 @@ public class SourceAleatoire extends Source<Boolean> {
         this.informationEmise = this.informationGeneree;
     }
 
+    /**
+     * un constructeur permettant de générer une information binaire
+     * aléatoire, en fixant le seed du générateur aléatoire
+     *
+     * @param taille le nombre de bits du message généré
+     * @param seed la graine du générateur aléatoire
+     */
     public SourceAleatoire(int taille, int seed) {
         super();
         this.informationGeneree = new Information<Boolean>();
