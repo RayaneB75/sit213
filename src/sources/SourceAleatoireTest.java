aaa25d8b19d1f5087d8825e0fc185700e4a07000
Tchadel Icard <tchadel.icard@imt-atlantique.net>
2023-09-06 22:42:00 +0200

    add unit tests for SourceAleatoire class

---
+++ src/sources/SourceAleatoireTest.java
@@ -0,0 +1,26 @@
+package sources;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SourceAleatoireTest {
+    @Test
+    public void testSourceAleatoire() {
+        SourceAleatoire s1 = new SourceAleatoire(10);
+        assertTrue(s1.informationGeneree.nbElements() == 10);
+        s1 = new SourceAleatoire(10, 0);
+        assertTrue(s1.informationGeneree.nbElements() == 10);
+        SourceAleatoire s2 = new SourceAleatoire(10, 0);
+        int diff = 0;
+
+        for (Boolean b1 : s1.informationGeneree) {
+            for (Boolean b2 : s2.informationGeneree) {
+                if (b1 != b2) {
+                    diff++;
+                }
+            }
+        }
+        assertTrue(diff > 0);
+    }
+}
