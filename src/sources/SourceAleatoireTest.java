904550d9b601e707ba7232d54369a3cdd235e4ea
Tchadel Icard <tchadel.icard@imt-atlantique.net>
2023-09-07 18:11:02 +0200

    use equals method in Source tests

--- src/sources/SourceAleatoireTest.java
+++ src/sources/SourceAleatoireTest.java
@@ -12,13 +12,6 @@ public class SourceAleatoireTest {
         s1 = new SourceAleatoire(10, 10);
         assertTrue("La source n'a pas généré 10 symboles", s1.informationGeneree.nbElements() == 10);
         SourceAleatoire s2 = new SourceAleatoire(10, 10);
-
-        int diff = 0;
-        for (int i = 0; i < s1.informationGeneree.nbElements(); i++) {
-            if (s1.informationGeneree.iemeElement(i) != s2.informationGeneree.iemeElement(i)) {
-                diff++;
-            }
-        }
-        assertTrue("Deux source avec la même entrée donnent des sorties différentes", diff == 0);
+        assertTrue("Deux source avec la même entrée donnent des sorties différentes", s1.informationGeneree.equals(s2.informationGeneree));
     }
 }
--- src/sources/SourceFixeTest.java
+++ src/sources/SourceFixeTest.java
@@ -13,16 +13,10 @@ public class SourceFixeTest {
         assertTrue("La source n'a pas généré 10 symboles", s1.informationGeneree.nbElements() == 10);
         SourceFixe s2 = new SourceFixe(10, "1000101100");
 
-        int diff = 0;
-        for (int i = 0; i < s1.informationGeneree.nbElements(); i++) {
-            if (s1.informationGeneree.iemeElement(i) != s2.informationGeneree.iemeElement(i)) {
-                diff++;
-            }
-        }
-        assertTrue("Deux source avec la même entrée donnent des sorties différentes", diff == 0);
+        assertTrue("Deux source avec la même entrée donnent des sorties différentes", s1.informationGeneree.equals(s2.informationGeneree));
         s1 = new SourceFixe(7, "1000101100");
         assertTrue(s1.informationGeneree.nbElements() == 7);
-        diff = 0;
+        int diff = 0;
         for (int i = 0; i < 7; i++) {
             if (s1.informationGeneree.iemeElement(i) != ("1000101100".charAt(i) == '1')) {
                 diff++;
