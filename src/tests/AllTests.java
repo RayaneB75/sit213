68dc783c3a2825973935be5a3db3254039c37a32
Tchadel Icard <tchadel.icard@imt-atlantique.net>
2023-09-07 18:44:29 +0200

    patch tests

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -4,13 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import destinations.DestinationFinale;
+import destinations.DestinationFinaleTest;
 import sources.SourceAleatoireTest;
 import sources.SourceFixeTest;
 import transmetteurs.TransmetteurParfaitTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DestinationFinale.class, SourceAleatoireTest.class, SourceFixeTest.class, TransmetteurParfaitTest.class })
+@SuiteClasses({ DestinationFinaleTest.class, SourceAleatoireTest.class, SourceFixeTest.class, TransmetteurParfaitTest.class })
 public class AllTests {
 
 }
