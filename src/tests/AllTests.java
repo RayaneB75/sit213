e64ba720914918199510bd296f9e6844a1d0b8ac
Tchadel Icard <tchadel.icard@imt-atlantique.net>
2023-09-07 18:40:41 +0200

    add new tests to AllTests

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import destinations.DestinationFinale;
 import sources.SourceAleatoireTest;
 import sources.SourceFixeTest;
+import transmetteurs.TransmetteurParfaitTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SourceFixeTest.class, SourceAleatoireTest.class })
+@SuiteClasses({ DestinationFinale.class, SourceAleatoireTest.class, SourceFixeTest.class, TransmetteurParfaitTest.class })
 public class AllTests {
 
 }
