28d71baf5adbb958a8b409febc71c24c0b6b2de0
Dan Achille <achille.dan@protonmail.com>
2023-09-19 19:51:18 +0200

    JavaDOC Pour (DE)ModulateurRZ + Tests JUNIT 2&3

--- src/tests/TransmetteurParfaitTest.java
+++ src/tests/TransmetteurParfaitTest.java
@@ -1,15 +1,24 @@
+/**
+ * Cette classe représente un ensemble de tests JUnit pour la classe TransmetteurParfait.
+ * Elle vérifie le comportement des méthodes emettre et recevoir de la classe TransmetteurParfait
+ * en connectant une source aléatoire à une destination finale.
+ */
 package tests;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-
 import destinations.DestinationFinale;
 import information.InformationNonConformeException;
 import sources.SourceAleatoire;
 import transmetteurs.TransmetteurParfait;
 
 public class TransmetteurParfaitTest {
+
+    /**
+     * Test unitaire pour la méthode emettre de la classe TransmetteurParfait.
+     * Il vérifie si l'information émise par un transmetteur parfait correspond
+     * à l'information reçue par une destination finale.
+     */
     @Test
     public void testEmettre() {
         SourceAleatoire s = new SourceAleatoire(10);
@@ -22,10 +31,15 @@ public class TransmetteurParfaitTest {
         } catch (InformationNonConformeException e) {
             System.out.println(e.getMessage());
         }
-        assertTrue("L'information émise ne correspond pas à 'information reçue",
+        assertTrue("L'information émise ne correspond pas à l'information reçue",
                 t.getInformationEmise().equals(d.getInformationRecue()));
     }
 
+    /**
+     * Test unitaire pour la méthode recevoir de la classe TransmetteurParfait.
+     * Il vérifie si l'information émise par une source aléatoire correspond
+     * à l'information reçue par un transmetteur parfait.
+     */
     @Test
     public void testRecevoir() {
         SourceAleatoire s = new SourceAleatoire(10);
