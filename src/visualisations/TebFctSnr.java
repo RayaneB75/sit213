21aae2a8f5dbf13f8e5995d0ad44f3e49de1a586
Dan Achille <achille.dan@protonmail.com>
2023-09-21 16:55:56 +0200

    Creation des javadoc pour TebFctSnr.java et VueCourbe.java

--- src/visualisations/TebFctSnr.java
+++ src/visualisations/TebFctSnr.java
@@ -1,3 +1,12 @@
+/**
+ * Cette classe Java réalise des simulations de transmission de données à différents niveaux de rapport signal/bruit (SNR)
+ * et enregistre les taux d'erreur binaire (TEB) résultants dans un fichier CSV.
+ *
+ * Le script effectue 50 simulations en variant le SNR de -40 dB à 15 dB et enregistre les résultats dans un fichier CSV
+ * Chaque simulation utilise un simulateur (classe Simulateur) pour générer et transmettre des données.
+ *
+ * Pour chaque valeur de SNR, le script enregistre le SNR et le TEB correspondant dans le fichier CSV.
+ */
 package visualisations;
 
 import java.io.FileWriter;
@@ -9,6 +18,11 @@ import simulateur.Simulateur;
 
 public class TebFctSnr {
 
+    /**
+     * La méthode principale qui réalise les simulations de TEB en fonction du SNR.
+     *
+     * @param args Les arguments de ligne de commande (non utilisés dans ce script).
+     */
     public static void main(String[] args) {
         List<Integer> snrValues = new ArrayList<>();
         List<Float> tebValues = new ArrayList<>();
@@ -45,6 +59,12 @@ public class TebFctSnr {
         }
     }
 
+    /**
+     * Cette méthode effectue une simulation avec le SNR spécifié et retourne le TEB résultant.
+     *
+     * @param snr Le rapport signal/bruit (SNR) spécifié en dB.
+     * @return Le taux d'erreur binaire (TEB) calculé pour la simulation.
+     */
     private static float runSimulation(int snr) {
         float teb = 0;
         try {
